8d680dfd8b9bc70f39a8cde25e550aa2eb3b3900
Jada Hundley <jadahundley@gmail.com>
2026-06-01 20:24:18 -0500

    completed anonymous functions practice exercises.

--- tutorials/09-more-functions/anonymous-functions/exercises/practice.js
+++ tutorials/09-more-functions/anonymous-functions/exercises/practice.js
@@ -15,6 +15,7 @@ console.log("Sending outbound radar frequency sweep...");
        that logs: "📡 Echo received: Object detected at 12,000km."
     2. A delay time of 3000 milliseconds (3 seconds).
 */
+setTimeout(() => console.log("📡 Echo received: Object detected at 12,000km."), 3000);
 
 /** EXERCISE 2: EMERGENCY KLAXON TIMER **/
 
@@ -25,3 +26,4 @@ console.log("Airlock seal breach detected! Charging secondary containment...");
     Pass in a succinct anonymous arrow function that logs:
     "🚨 Emergency magnetic shields are now online!"
 */
+setTimeout(() => console.log("🚨 Emergency magnetic shields are now online!"), 1000);
